ccaf0cf680d734e1137e060aa90eee2c397cf2f5
venkatesh <nagireddyvenkatesh@gmail.com>
2020-09-08 15:52:07 -0400

    Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Amazon Clone !!! </h1>
+      <Header />
+      {/* Home */}
     </div>
   );
 }

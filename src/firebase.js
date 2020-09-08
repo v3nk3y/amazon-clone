49559b4c7cc9fecf7743dfa171a263362fcf0fb7
venkatesh <nagireddyvenkatesh@gmail.com>
2020-09-08 15:09:10 -0400

    Clean up for starter project

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCRtOESdoo9MMzO1qK8yjG-hCZfMADqd18",
+    authDomain: "challenge-79bba.firebaseapp.com",
+    databaseURL: "https://challenge-79bba.firebaseio.com",
+    projectId: "challenge-79bba",
+    storageBucket: "challenge-79bba.appspot.com",
+    messagingSenderId: "173813674206",
+    appId: "1:173813674206:web:85239efbf6f42b450f524f"
+  };

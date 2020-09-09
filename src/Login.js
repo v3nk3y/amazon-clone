9ec039516c9d12749bf0fd203d890023346050d0
venkatesh <nagireddyvenkatesh@gmail.com>
2020-09-09 16:58:52 -0400

    Login page UI implementation

---
+++ src/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefualt();
+  };
+  const register = (e) => {
+    e.preventDefualt();
+  };
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="login__signInButton"
+            onClick={signIn}
+          >
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to this "AMAZON CLONE" Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-BAsed Ads Notice.
+        </p>
+        <button
+          type="button"
+          className="login__registerButton"
+          onClick={register}
+        >
+          Create your Account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;

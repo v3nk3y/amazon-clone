9324a69503594ac8ea4f6fd0f5ad0d5454721853
venkatesh <nagireddyvenkatesh@gmail.com>
2020-09-09 15:20:03 -0400

    Basket update and subtotal calc implemnted

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
 
 function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="subtotal">
       <CurrencyFormat
@@ -10,7 +13,7 @@ function Subtotal() {
           <>
             <p>
               {/* TODO */}
-              Subtotal (0 items): <strong>0</strong>
+              Subtotal ({basket?.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -18,7 +21,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0} //TODO
+        value={getBasketTotal(basket)} //TODO
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}

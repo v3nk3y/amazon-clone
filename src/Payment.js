0defd6c6f1fea028102c927f2eaf3fbab48f8282
venkatesh <nagireddyvenkatesh@gmail.com>
2020-09-10 17:12:41 -0400

    Payment/Checkout page

---
+++ src/Payment.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Payment.css";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import { Link } from "react-router-dom";
+
+function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length} items</Link>)
+        </h1>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>523 Mariener Terr</p>
+            <p>Toronto, CA</p>
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket?.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+            {/* Stripe integration goes here..... */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;

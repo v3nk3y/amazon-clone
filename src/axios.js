359a060cac7dfee986ce711bb9238e47ccba7f9d
venkatesh <nagireddyvenkatesh@gmail.com>
2020-09-14 14:58:44 -0400

    update axios api url for firebasecloud functions

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/challenge-79bba/us-central1/api", // The API URL (CLOUD function)
+  baseURL: "https://us-central1-challenge-79bba.cloudfunctions.net/api", // The API URL (CLOUD function)
+  // baseURL: "http://localhost:5001/challenge-79bba/us-central1/api",
 });
 
 export default instance;
